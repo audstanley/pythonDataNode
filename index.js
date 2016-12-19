9ef997b2970ffbaeffcf605624e114d38d890fc9
Richard Stanley <audstanley@gmail.com>
2016-12-18 23:32:28 -0800

    added graceful shutdown to index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express')
-var app = express()
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+  console.log('pid is ' + process.pid);
+});
+
+process.on('SIGTERM', function () {
+  server.close(function () {
+    process.exit(0);
+  });
+});
