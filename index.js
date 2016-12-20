746a1ebc6fc72d1a1087920a75f69abd6b6472a3
Richard Stanley <audstanley@gmail.com>
2016-12-19 18:14:53 -0800

    added middleware to index.js, jade, and body-parser modules were added to the package.json and some uploaded files were added to the new uploads folder.
    Also added my first Jade file: /views/indedex.jade

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var express = require('express')
+var express = require('express');
+var multer = require('multer'),
+    bodyParser = require('body-parser'),
+    path = require('path');
+
+
 var app = express();
+app.use(bodyParser.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
+
+
+app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req, res) {
+    console.log(req.body); //form fields
+    console.log(req.file); //form files
+    res.status(204).end();
+})
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
