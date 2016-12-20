324ecbcb81c2b9b94edfcf437e4ddde6f535c51b
Richard Stanley <audstanley@gmail.com>
2016-12-20 11:09:03 -0800

    Update commandsForGit.sh

--- commandsForGit.sh
+++ commandsForGit.sh
@@ -15,7 +15,9 @@ git clone https://github.com/audstanley/pythonDataNode;
 
 
 
-# 1. Always pull new changes before you commit your changes to the web.
+# 1. Always pull new changes(from github) before you commit your changes to the web.
+# it's good to get into the practice of addeding/pulling/commiting/pushing as often 
+# as you normally save your file.
 git pull;
 
 
@@ -30,13 +32,14 @@ git add someFileName.txt someOtherFile.txt somethingElse.txt
 
 
 # 3. With git installed on your system you need to "commit" your added files to git.
-# This is a little confusing because you have a LOCAL repository (when you instealled git), 
+# This is a little confusing because you have a LOCAL repository (when you installed git), 
 # and there is also a REMOTE repository (github).  Before you can PUSH to the REMOTE repo,
 # you have to commit your local changes.
 git commit;
 # right after you run git commit, a text document will appear with comments at the bottom 
 # of the page.  In the comment section you'll see the files that you have added and the files
-# that will also be ignored.
+# that will also be ignored. Make sure you type SOMETHING, like: "I added someFile.txt, and changed
+# whatever in the index.js file."
 
 
 
