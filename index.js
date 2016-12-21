b0f4d12632093aa1c54ff34c496c7f2744bead33
Richard Stanley <audstanley@gmail.com>
2016-12-21 11:59:28 -0800

    .

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/users/:id', function (req, res) {
 app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req, res) {
     console.log(req.body); //form fields
     console.log(req.file); //form files
-    res.render.(__dirname + req.file.path)
+    res.render(__dirname + req.file.path)
     res.status(204).end();
 })
 
