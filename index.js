6c1443f6be43bfa1b191c169f8eeeeeac88cf0ef
Richard Stanley <audstanley@gmail.com>
2016-12-21 21:51:28 -0800

    .

--- index.js
+++ index.js
@@ -45,11 +45,11 @@ app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req, res) {
     //console.log(req.file.originalname); //form files
     var extension = req.file.originalname.toLowerCase().match(regexFileExtension)[0]
     //console.log("The extension is: " + extension)
-    fs.rename(req.file.path, req.file.path + "." + extension, function(err) {
+    fs.rename(req.file.path, req.file.path + extension, function(err) {
       if(err) throw err
-      else console.log('File upload successful: ' + req.file.originalname + '\n    To: ' + req.file.filename + "." + extension)
+      else console.log('File upload successful: ' + req.file.originalname + '\n    To: ' + req.file.filename + extension)
     })
-    res.redirect(req.file.path + "." + extension)
+    res.redirect(req.file.path + extension)
     res.status(204).end();
 })
 
