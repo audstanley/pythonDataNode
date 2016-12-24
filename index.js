c1cc62aedac411231d17c7535f8b4805880de378
Richard Stanley <audstanley@gmail.com>
2016-12-23 19:37:42 -0800

    .

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ app.get('/', function (req, res) {
   var jvPath = __dirname + '/public/views.json'
   var readJViews = JSON.parse(fs.readFileSync(jvPath)).hits
   var n = Number.parseInt(readJViews, 10) + 1
+  fs.writeFileSync(jvPath, '{ \"hits\": \"' + n + '\" }')
   console.log(n)
   res.render('index', {views: n});
-  //console.log("Main Page Viewed.")
+  console.log("Main Page Viewed.")
 });
 
 app.get('/uploads', function (req, res) {
