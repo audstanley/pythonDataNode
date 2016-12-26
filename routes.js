f4d8858f2aa9a5bada8c3f8554f58e78f8cae9be
Richard Stanley <audstanley@gmail.com>
2016-12-26 11:58:50 -0800

    .

--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var main = require('./routes/index');
 
-concol.log('Got to routes.js')
-router.get('/', main.getIndex));
-router.route('/', main.postIndex);
+console.log('Got to routes.js')
+router.get('/', main.getIndex()));
+router.route('/', main.postIndex());
 
 module.exports = router;
