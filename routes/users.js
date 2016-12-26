d449c814bc8518aa344652521b6695c3f739d7ca
Richard Stanley <audstanley@gmail.com>
2016-12-26 14:04:32 -0800

    .

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ let appRoot = process.env.PWD
 
 module.exports = {
   getUser: function (req, res) {
-    console.log(req)
+    console.log(req.params.id)
     res.render(appRoot + '/views/users', { title: "Hey ", message: "Hello there!", uid: req.params.id })
   }
 }
