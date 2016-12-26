5d0b358b9528ea47f143414b4ed26d84c095da55
Richard Stanley <audstanley@gmail.com>
2016-12-26 13:48:19 -0800

    .

--- routes.js
+++ routes.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var users = require('./routes/users');
 
 console.log('Got to routes.js')
-router.get('/', users.getUser);
+router.get('/users', users.getUser);
 
 module.exports = router;
