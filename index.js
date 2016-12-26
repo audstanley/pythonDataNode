8337fe080123ad7b5f6c0409c0be220934dfe091
Richard Stanley <audstanley@gmail.com>
2016-12-26 10:35:52 -0800

    .

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var multer = require('multer'),
     path = require('path'),
     fs = require('fs');
 
-var app = express();
+const app = express();
 
 //middleware
+app.use('/', require(__dirname, 'routes/index.js'));
 app.use('/uploads/', express.static(path.join(__dirname, 'uploads')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
@@ -14,16 +15,6 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
-app.get('/', function (req, res) {
-  let jvPath = __dirname + '/public/views.json'
-  let readJViews = JSON.parse(fs.readFileSync(jvPath)).hits
-  let n = Number.parseInt(readJViews, 10) + 1
-  fs.writeFileSync(jvPath, '{ \"hits\": \"' + n + '\" }')
-  console.log(n + " from: " + req.ip)
-  res.render('index', {views: n.toLocaleString()});
-  //console.log("Main Page Viewed.")
-});
 
 app.get('/uploads', function (req, res) {
 let uploadsPath = __dirname + '/uploads/'
@@ -41,22 +32,6 @@ app.get('/users/:id', function (req, res) {
   res.render('users', { title: "Hey ", message: "Hello there!", uid: req.params.id });
 });
 
-
-app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req, res) {
-    let regexFileExtension = /\.[0-9a-z]{1,5}$/;
-    //console.log(req.body); //form fields
-    //console.log(req.file); //form files
-    //console.log(req.file.originalname); //form file
-    var extension = req.file.originalname.toLowerCase().match(regexFileExtension)[0]
-    //console.log("The extension is: " + extension)
-    fs.rename(req.file.path, req.file.path + extension, function(err) {
-      if(err) throw err
-      else console.log('File upload successful: ' + req.file.originalname +
-                     '\n                        To: ' + req.file.filename + extension)
-    })
-    res.redirect('/uploads')
-    res.status(204).end();
-})
 
 
 app.listen(3000, function () {
