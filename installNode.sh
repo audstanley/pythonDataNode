9bcd472be9cc4752a6d096cbf3b8e4615f024753
Richard Stanley <audstanley@gmail.com>
2016-12-27 13:44:41 -0800

    Update installNode.sh

--- installNode.sh
+++ installNode.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-
+PICHIP=$(uname -m);
 if [ "$EUID" -ne 0 ]
-        then echo "You need to install as root by using sudo ./installNode";
+        then echo "You need to install as root by using sudo ./Install-Node.sh";
         exit
-else cd ~/ && mkdir temp && cd temp && wget https://nodejs.org/dist/latest-v7.x/node-v7.2.1-linux-armv7l.tar.gz;
-tar -xzf node-v7.2.1-linux-armv7l.tar.gz;
+else LINKTONODE=$(curl -G https://nodejs.org/dist/latest-v7.x/ | awk '{print $2}' | grep -P 'href=\"node-v7\.\d{1,}\.\d{1,}-linux-'$PICHIP'\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//');
+# curl -G https://nodejs.org/dist/latest-v7.x/ | awk '{print $2}' | grep -P 'href=\"node-v7\.\d{1,}\.\d{1,}-linux-armv7l\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//'
+NODEFOLDER=$(echo $LINKTONODE | sed 's/.tar.gz/\//');
+#Shell script created by @audstanley
+#This will work specifically for a RaspberryPi 2/3 ARM7.
+#Next, Creates directory for downloads, and downloads node 7.x
+cd ~/ && mkdir tempNode && cd tempNode && wget https://nodejs.org/dist/latest-v7.x/$LINKTONODE;
+tar -xzf $LINKTONODE;
 #Remove the tar after extracing it.
-sudo rm node-v7.2.1-linux-armv7l.tar.gz;
+rm $LINKTONODE;
+#remove older version of node:
+rm -R -f /opt/nodejs/;
+#remove symlinks
+rm /usr/bin/node /usr/sbin/node /sbin/node /sbin/node /usr/local/bin/node /usr/bin/npm /usr/sbin/npm /sbin/npm /usr/local/bin/npm;
 #This next line will copy Node over to the appropriate folder.
-sudo mv node-v7.2.1-linux-armv7l/ /opt/nodejs/;
-#This line will remove the nodeJs we downloaded.
-sudo rm -R node-v7.2.1-linux-armv7l.tar.gz/* && sudo rmdir node-v7.2.1-linux-armv7l.tar.gz/;
+mv /root/tempNode/$NODEFOLDER /opt/nodejs/;
+#This line will remove the nodeJs tar we downloaded.
+rm -R -f /root/tempNode/$LINKTONODE/;
 #Create symlinks to node && npm
 sudo ln -s /opt/nodejs/bin/node /usr/bin/node;
 sudo ln -s /opt/nodejs/bin/node /usr/sbin/node;
@@ -20,4 +30,7 @@ sudo ln -s /opt/nodejs/bin/npm /usr/bin/npm;
 sudo ln -s /opt/nodejs/bin/npm /usr/sbin/npm;
 sudo ln -s /opt/nodejs/bin/npm /sbin/npm;
 sudo ln -s /opt/nodejs/bin/npm /usr/local/bin/npm;
+rm -R -f /root/tempNode/;
+#su pi;
+#cd ~/ && rm -R NodeJs-Raspberry-Pi-Arm7/;
 fi
